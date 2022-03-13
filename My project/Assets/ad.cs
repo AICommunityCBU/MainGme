72c29a492d05d8d60d639c21d7258537a9b6b2c1
Muratcan <73342591+murtican16@users.noreply.github.com>
2022-03-13 17:48:22 +0300

    cre

---
+++ My project/Assets/ad.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //hello world
+    //Emre cakmak
+    void Start()
+    {
+        
+    }
+    // Bak hele bK BAK AKBAKBAKBAKBKABKA
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
