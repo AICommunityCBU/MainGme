d54591efbd9779412025c87e88d4abc2f276f2d9
emrcakmak <emrecakmak123q@gmail.com>
2022-03-14 15:16:28 +0300

    .

--- MyProject2/Assets/Araba.cs
+++ MyProject2/Assets/Araba.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Araba : MonoBehaviour
+public class araba : MonoBehaviour
 {
-
-
-    private void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        Movement();
+        
     }
-    public void Movement()
-    {
 
+    // Update is called once per frame
+    void Update()
+    {
+            //araba durdu
     }
 }
